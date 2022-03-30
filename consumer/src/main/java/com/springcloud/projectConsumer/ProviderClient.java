4fa5f829d9132189dbc46b59f194897afb9a7576
chaochao <457803757@qq.com>
2022-03-30 17:19:35 +0800

    微服务

---
+++ consumer/src/main/java/com/springcloud/projectConsumer/ProviderClient.java
@@ -0,0 +1,12 @@
+package com.springcloud.projectConsumer;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value="provider")
+public interface ProviderClient {
+
+    @GetMapping("/hello")
+    String hello(@RequestParam(value = "name", defaultValue = "NACOS", required = false) String name);
+}
